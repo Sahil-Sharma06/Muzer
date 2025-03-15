5051906d486a3c3342d9f89b9eb4b39257a8083b
Sahil-Sharma06 <116885079+Sahil-Sharma06@users.noreply.github.com>
2025-03-15 18:57:21 +0530

    first commit

---
+++ app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,12 @@
+import NextAuth from "next-auth";
+import GoogleProviders from "next-auth/providers/google";
+const handler = NextAuth({
+    providers:[
+        GoogleProviders({
+            clientId: process.env.GOOGLE_CLIENT_ID ?? "",
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET ?? ""
+        })
+    ]
+})
+
+export {handler as GET, handler as POST}
